0fb1ffc48b3dace02e7c4d98fe4a992028c2fc98
Cody Tanner <ctanner@instructure.com>
2015-09-22 10:59:27 -0600

    fix task api controller

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,8 +1,6 @@
 class Api::V1::TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
-  # GET /tasks
-  # GET /tasks.json
   def index
     if params[:application_id].present?
       @tasks = Application.find(params[:application_id]).tasks
@@ -12,59 +10,31 @@ class Api::V1::TasksController < ApplicationController
     render json: @tasks
   end
 
-  # GET /tasks/1
-  # GET /tasks/1.json
   def show
     render json: @task
   end
 
-  # GET /tasks/new
-  def new
-    @task = Task.new
-  end
-
-  # GET /tasks/1/edit
-  def edit
-  end
-
-  # POST /tasks
-  # POST /tasks.json
   def create
     @task = Task.new(task_params)
-
-    respond_to do |format|
       if @task.save
-        format.html { redirect_to @task, notice: 'Task was successfully created.' }
-        format.json { render :show, status: :created, location: @task }
+        render json: @task, status: :created
       else
-        format.html { render :new }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
+        render json: @task.errors, status: :unprocessable_entity
       end
     end
   end
 
-  # PATCH/PUT /tasks/1
-  # PATCH/PUT /tasks/1.json
   def update
-    respond_to do |format|
-      if @task.update(task_params)
-        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
-        format.json { render :show, status: :ok, location: @task }
-      else
-        format.html { render :edit }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
+    if @task.update(task_params)
+      render json: @task status: :ok
+    else
+      render json: @task.errors, status: :unprocessable_entity
     end
   end
 
-  # DELETE /tasks/1
-  # DELETE /tasks/1.json
   def destroy
     @task.destroy
-    respond_to do |format|
-      format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    render json: :no_content, status: :ok
   end
 
   private
